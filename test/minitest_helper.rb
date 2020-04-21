67216dd29f449fdfe38ca36082ec555859d272da
Andrei Popa <andrei.popa@2checkout.com>
2020-04-21 13:30:41 +0300

    Bumped version

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,8 +2,8 @@ require "minitest/autorun"
 require "twocheckout"
 
 Twocheckout::API.credentials = {
-    :username => 'test_api_250111206876',
-    :password => 'Qwerty123',
-    :seller_id => '250111206876',
-    :private_key => '1D336D05-05AA-40A4-AEF6-DF58D222E6F4',
+    :username => 'CREDENTIALS HERE',
+    :password => 'CREDENTIALS HERE',
+    :seller_id => 'CREDENTIALS HERE',
+    :private_key => 'CREDENTIALS HERE',
 }
